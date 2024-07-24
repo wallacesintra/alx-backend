af42c86024d090cbdf7651a6f2d29b6a887170c0
wallacesintra <wallvxwallace962@gmail.com>
2024-07-25 00:15:21 +0300

    cache dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -21,7 +21,7 @@ class BasicCache(BaseCaching):
         the item value for the key key.
         """
 
-        if (key is not None and item is not None):
+        if (key is not None or item is not None):
             self.cache_data[key] = item
 
     def get(self, key):
