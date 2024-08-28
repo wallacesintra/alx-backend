54e0492eaa372e8409f425ac1ef378ae3b5d2da9
wallacesintra <wallvxwallace962@gmail.com>
2024-08-28 12:40:47 +0300

    connecting to redis server

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+#!/usr/bin/yarn dev
+
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log(`Redis client connected to the server`);
+})
+
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: `, error.toString())
+})
